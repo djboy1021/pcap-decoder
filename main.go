3ab2b831984eb47a75a68dea4f60972d3e866d21
Brendon Dulam <brendon.dulam@veoneer.com>
2019-11-08 09:52:18 +0900

    help screen

--- main.go
+++ main.go
@@ -21,6 +21,41 @@ func printSummary(pcapFile *string, outputPath *string, startFrame *int, endFram
 	fmt.Println("Execution Time:", (*endTime).Sub(*startTime))
 }
 
+func validatePaths(pcapFile *string, outputPath *string, mkdirp bool) {
+	// Check if PCAP file exists
+	if !path.Exists(*pcapFile) {
+		panic(*pcapFile + " does not exist")
+	}
+
+	// Check if Output path exists
+	if !path.Exists(*outputPath) {
+		if mkdirp {
+			fmt.Println("Output Path will be generated")
+		} else {
+			panic(*outputPath + " does not exist")
+		}
+	}
+
+}
+
+func printHelp() {
+	fmt.Println("Usage: ./pcap-decoder.exe <arguments>")
+	fmt.Println("")
+	fmt.Println("where <arguments> is one of:")
+	fmt.Println("    --pcapFile    <string, full path of the pcap file>")
+	fmt.Println("    --outputPath  <string, path of the output files>")
+	fmt.Println("    --mkdirp      <boolean, true/false>")
+	fmt.Println("    --startFrame  <integer, 0 for first frame>")
+	fmt.Println("    --endFrame    <integer, -1 for last frame>")
+	fmt.Println("    --mkdirp      <integer>")
+	fmt.Println("    --isStatsOnly <boolean>")
+	fmt.Println("    --json        <boolean>")
+	fmt.Println("")
+	fmt.Println("Sample:")
+	fmt.Println("./pcap-decoder.exe --pcapFile V:/JP01/DataLake/Common_Write/KEEP_Magic_Hat_GT/RTB_DC/External_Shared_Data/city.pcap --outputPath V:/JP01/DataLake/Common_Write/KEEP_Magic_Hat_GT/RTB_DC/External_Shared_Data --mkdirp false --startFrame 0 --endFrame 12 --json true")
+	fmt.Println("")
+}
+
 func main() {
 	// Default values
 	pcapFile := ""
@@ -33,20 +68,6 @@ func main() {
 
 	// Get Commandline arguments
 	cli.GetArgs(&pcapFile, &outputPath, &startFrame, &endFrame, &mkdirp, &isStatsOnly, &isSaveAsJSON)
-
-	// Check if PCAP file exists
-	if !path.Exists(pcapFile) {
-		panic(pcapFile + " does not exist")
-	}
-
-	// Check if Output path exists
-	if !path.Exists(outputPath) {
-		if mkdirp {
-			fmt.Println("Output Path will be generated")
-		} else {
-			panic(outputPath + " does not exist")
-		}
-	}
 
 	// Allocate number of workers
 	totalWorkers := uint8(runtime.NumCPU() / 2)
@@ -64,12 +85,19 @@ func main() {
 		pcapStats := parsepcap.GetStats(&pcapFile)
 		jsonBin, _ := json.Marshal(pcapStats)
 		fmt.Println("Stats:", string(jsonBin))
-	} else {
+	} else if len(pcapFile) > 0 && len(outputPath) > 0 && isSaveAsJSON {
+		// Check if paths are valid
+		validatePaths(&pcapFile, &outputPath, mkdirp)
+
+		// Check IP addresses of the packets
 		channels := parsepcap.GetIP4Channels(&pcapFile)
 		parsepcap.ParsePCAP(&pcapFile, &outputPath, totalWorkers, startFrame, endFrame, channels, isSaveAsJSON)
-	}
-	endTime := time.Now()
 
-	// Display Summary
-	printSummary(&pcapFile, &outputPath, &startFrame, &endFrame, &startTime, &endTime, &mkdirp, &isStatsOnly)
+		// Display Summary
+		endTime := time.Now()
+		printSummary(&pcapFile, &outputPath, &startFrame, &endFrame, &startTime, &endTime, &mkdirp, &isStatsOnly)
+	} else {
+		printHelp()
+	}
+
 }
