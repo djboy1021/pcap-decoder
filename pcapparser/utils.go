aa71f9232418e76baeabf66714e7ad8c4719f418
Brendon Dulam <brendon.dulam@veoneer.com>
2019-11-23 20:35:08 +0900

    removed getElevationAngle

--- pcapparser/utils.go
+++ pcapparser/utils.go
@@ -32,20 +32,6 @@ func getAzimuthGap(currAzimuth uint16, nextAzimuth uint16) uint16 {
 }
 
 // returns the elevation angle in degrees
-func getElevationAngle(productID byte, rowIndex uint8) float64 {
-	var elevAngle float64
-
-	switch productID {
-	case 0x22:
-		elevAngle = float64(dictionary.VLP16ElevationAngles[rowIndex%16]) / 1000
-	case 0x28:
-		elevAngle = float64(dictionary.VLP32ElevationAngles[rowIndex]) / 1000
-	}
-
-	return elevAngle
-}
-
-// returns the elevation angle in degrees
 func getRawElevationAngle(productID byte, rowIndex uint8) int16 {
 	var elevAngle int16
 
